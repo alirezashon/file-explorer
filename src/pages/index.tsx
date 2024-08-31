63df976f75c56a9336f15f79aafd176401bb6892
Alireza <103531312+alirezashon@users.noreply.github.com>
2024-08-31 15:11:36 +0330

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,3 @@
-// import React from 'react'
-// import FolderComponent from '../components/Folder'
-// import { useExplorer } from '../context/Explorer'
-
-// const HomePage: React.FC = () => {
-//   const { root } = useExplorer()
-
-//   return (
-//     <div>
-//       <FolderComponent folder={root} />
-//     </div>
-//   )
-// }
-
-// const App: React.FC = () => {
-//   return (
-//       <HomePage />
-//   )
-// }
-
-// export default App
-
-// pages/index.tsx
-import React from 'react'
 import FileExplorer from '../components/FileExplorer'
 import { FileExplorerProvider } from '../context/Explorer'
 
